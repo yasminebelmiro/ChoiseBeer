638940d79a1be19a5492e36571f551a0d0bd43fa
YasmineBelmiro <devyasminebelmiro@gmail.com>
2024-09-24 08:24:27 -0300

    upadate files

--- app/src/main/java/ifgoiano/urt/choisebeer/BuscarCervejaActivity.kt
+++ app/src/main/java/ifgoiano/urt/choisebeer/BuscarCervejaActivity.kt
@@ -14,30 +14,18 @@ class BuscarCervejaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buscar_cerveja)
-
-        // Inicializando a classe ExpertCerveja
         cervejaExpert = ExpertCerveja()
-
-        // Ligando os componentes da UI
         tipoCervejaSpinner = findViewById(R.id.tipo_cerveja_spinner)
         mostrarMarcas = findViewById(R.id.marcas_text_view)
         buscarMarcasButton = findViewById(R.id.buscar_button)
-
-        // Definindo o comportamento do botão
         buscarMarcasButton.setOnClickListener {
             buscarMarcas()
         }
     }
 
-    // Método para buscar e mostrar as marcas
     private fun buscarMarcas() {
-        // Obtendo o tipo de cerveja selecionado
         val tipoSelecionado = tipoCervejaSpinner.selectedItem.toString()
-
-        // Buscando as marcas para o tipo de cerveja
         val marcas = cervejaExpert.getMarcas(tipoSelecionado)
-
-        // Mostrando as marcas no campo de texto
         mostrarMarcas.text = marcas.joinToString(separator = "\n")
     }
 }
